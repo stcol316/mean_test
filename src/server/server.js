a246cf5f7926a9f4f86d43ac0aa1b9313efba01c
ZEN\Steve <metgenesis@hotmail.co.uk>
2020-08-28 22:53:10 +0100

    server.js done

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,38 @@
 const express = require('express');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 const app = express();
-
-app.listen(3000, () => {
-  console.log('Server started!')
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-var bookingApi = require('./routes/bookingApi');
+app.get('/', (req, res) => res.send('All aboard the Server Express!'));
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Node Server started on ${port}!`)
+});
+
+const url = 'mongodb://localhost:27017/bookingsDb';
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.once('open', _ => {
+  console.log('Mongoose is loose:', url)
+});
+
+db.on('error', err => {
+  console.error('Mongoose is down:', err)
+});
+
+const bookingApi = require('./routes/bookingApi');
 
 app.use('/api/v1/booking', bookingApi);
 
